47f45203d8f3256b14c8855ba064e98900080c9d
Heejo Kang <comforter1809@gmail.com>
2018-11-20 14:54:49 +0900

    객체지향과 객체형식

---
+++ Smaple01/Object.js
@@ -0,0 +1,25 @@
+/*객체를 만드는 방식
+객체지향에서...
+(1) 객체란 무엇인가?
+
+자바스크립트에서의 오브젝트 = 해시테이블/해시맵/딕셔너리 등과 비슷한 자료구조를 지님?*/
+
+var obj = {};
+/*변수 == 객체가 있는 주소(?)
+자료형(이 메모리 크기만큼 자리를 만들었어) 변수명(이 주소에) = new 객체가 있어*/
+
+//변수의 선언방식과 사용방식//
+obj.name = "Junil";
+console.log(obj.name);
+
+var obj2 = new Object();
+obj2["name"] = "홍길동";
+console.log(obj2);
+
+//javascript에서 객체의 형태 키:값/집단 대응형태
+var person = 
+{
+    name: "홍길동",
+    age: 33,
+    address: {zipcode: 14500, city: "서울"}
+}
--- Smaple01/test.js
+++ Smaple01/test.js
@@ -1,5 +1,5 @@
 /*
-객체지향 프로그래밍? 무엇이 객체지향 프로그래밍인가? OOP의 개념이란?
+객체지향 프로그래밍? 무엇이 객체지향 프로그래밍인가? OOP의 개념이란?2
 절차지향 프로그래밍? C
 함수형 프로그래밍?*/
 var name = 'KHJ';
@@ -47,10 +47,19 @@ let time1 = 100;
 
 /*if문과 switch문의 결정적 차이
 (1) switch문에서 break문의 의미
-     */ 
+    
+    switch문에서 case는 모두 변수 or 상수값을 나타냄 (bool타입이 아님)
+    case조건값에 외부에서 접근이 가능한건가?
+    상황마다 다른 상수값을 리턴하는 함수식에 사용한다? 흠...???  
+    
+(2) if문은 상황마다 내용이 바뀌고 switch는 아니다?*/ 
 let action = "eat";
 switch (action) {
     case 'draw': console.log('draw'); break;
     case 'eat': console.log('eat'); break;
     default: console.log('nothing');
 }
+
+/* git bash를 사용하는 경우
+(1) git에서 지원하지 않는 기능을 사용할 때
+(2) 마우스 사용을 최소화 하기 위해*/
